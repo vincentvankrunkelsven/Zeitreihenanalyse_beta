c82d50b27401c2c6b1435d597fdcc0b138568a97
alexandergerber <AlexanderGerber@web.de>
2017-02-26 13:23:21 +0100

    Update requirements.r

--- requirements.r
+++ requirements.r
@@ -1,3 +1,3 @@
-utils::install.packages("AER", repos = "http://cran.us.r-project.org" )
-devtools::install_version("AER", repos = "http://cran.us.r-project.org", type="source")
+devtools::install_version("AER")
 devtools::install_version("xts") 
+devtools::install_version("ggplot2") 
