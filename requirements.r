b073371978861e0d7643b1d07738269755e2cd8f
alexandergerber <AlexanderGerber@web.de>
2017-02-26 13:06:23 +0100

    Update requirements.r

--- requirements.r
+++ requirements.r
@@ -1,3 +1,3 @@
-install.packages("AER")
+utils::install.packages("AER", repos = "http://cran.us.r-project.org" )
 devtools::install_version("AER", repos = "http://cran.us.r-project.org", type="source")
-
+devtools::install_version("xts")
